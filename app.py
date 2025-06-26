dd1f99f38d113652077837c164bbe4d073873da5
Master-t93 <takumi2222s@icloud.com>
2025-06-26 14:52:16 +0900

    app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 app = Flask(__name__)
 
 # ここに自分の値を貼り付け！
-CHANNEL_SECRET = 297e52f347a7b5346baffbacf8503f8b
-CHANNEL_ACCESS_TOKEN = jGdIeEGvFXp3+kXYmXHPl0SAxe9ME8Zq6poNv3OmTtJqHzuWBZ+pFnjbgyanMT8LanOuTaXg0OogkD9eBbtwhXmiqgYb3mJcjDEWPg6axW1xIH6nOIGsYXZpMNNK7Le/QN+vQJHkVuE5o5Jbs2lqVAdB04t89/1O/w1cDnyilFU=
+CHANNEL_SECRET = '297e52f347a7b5346baffbacf8503f8b'
+CHANNEL_ACCESS_TOKEN = 'jGdIeEGvFXp3+kXYmXHPl0SAxe9ME8Zq6poNv3OmTtJqHzuWBZ+pFnjbgyanMT8LanOuTaXg0OogkD9eBbtwhXmiqgYb3mJcjDEWPg6axW1xIH6nOIGsYXZpMNNK7Le/QN+vQJHkVuE5o5Jbs2lqVAdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
